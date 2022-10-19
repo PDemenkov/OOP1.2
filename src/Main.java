fba8c1822cdb1577ed2b1b419213f91f127a39a8
PDemenkov <guapnik@gmail.com>
2022-10-19 16:33:29 +0300

    ДЗ выполнено 3

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
 import Transport.Car;
+import Transport.Car.Key;
+import Transport.Car.Insurance;
+import java.time.Year;
 
 public class Main {
     public static void main(String[] args) {
         Car lada = new Car("Lada", "Grand", 1.7, "yellow",2015,"Russia");
-        lada.setRegNumber("в352оа348");
+        Car.Key trueEngineKey = new Car.Key();
+        lada.setKey(trueEngineKey);
+        lada.setRegNumber("вd52оа348");
+        System.out.println("lada.isCorrectRegNumber() = " + lada.isCorrectRegNumber());
+        lada.setSummerTyres(false);
         Car audi = new Car("Audi", "A8 50 L TDI quattro", 3.0, "black", 2020, "Germany");
         Car bwm = new Car("BMW", "Z8", 3.0, "black", 2021, "Germany");
         Car kia = new Car("Kia", "Sportage 4th",2.4,"red",2018,"South Korea");
         Car hyundai = new Car("Hyundai", "Avante", 1.6, "orange", 2016, "South Korea");
-        hyundai.printOut();
 
         printInfo(lada);
     }
@@ -24,7 +30,12 @@ public class Main {
                         ", тип кузова:" + car.getTypeOfBody() +
                         ", рег.номер: " + car.getRegNumber() +
                         ", количество мест " + car.getSeatsCount() +
-                        ", " + (car.isSummerTyres() ? "летняя" : "зимняя") + "Резина"
+                        ", " + (car.isSummerTyres() ? "летняя" : "зимняя") + "Резина" +
+                        ", " + (car.getKey().isStartWithoutKey() ? "безключевой доступ" : "ключевой доступ") +
+                        ", " + (car.getKey().isRemoteStartEngine() ? "удаленный запуск" : "обычный запуск") +
+                        ", номер страховки " +
+                        ", стоимость страховки"
+
         );
     }
 }
--- src/Transport/Car.java
+++ src/Transport/Car.java
@@ -1,14 +1,37 @@
 package Transport;
 
+import java.time.LocalDate;
+
 public class Car {
-   private final String brand;
-   private final String model;
+    public static class Key {
+        private final boolean remoteStartEngine;
+        private final boolean startWithoutKey;
+        public Key(boolean remoteStartEngine, boolean startWithoutKey) {
+                this.remoteStartEngine = remoteStartEngine;
+                this.startWithoutKey = startWithoutKey;
+        }
+        public Key() {
+            this(false, false);
+        }
+
+        public boolean isRemoteStartEngine() {
+            return remoteStartEngine;
+        }
+
+        public boolean isStartWithoutKey() {
+            return startWithoutKey;
+        }
+    }
+    private Insurance insurance;
+    private Key key;
+    private final String brand;
+    private final String model;
     double engineVolume;
     private String color;
     private final int productionYear;
-    private final  String productionCountry;
+    private final String productionCountry;
     private String gears;
-    private final  String typeOfBody;
+    private final String typeOfBody;
     private String regNumber;
     private final int seatsCount;
     private boolean summerTyres;
@@ -23,20 +46,22 @@ public class Car {
                String typeOfBody,
                String regNumber,
                int seatsCount,
-               boolean summerTyres) {
+               boolean summerTyres,
+               Key key,
+               Insurance insurance) {
         if (brand == null) {
             this.brand = "default";
-        }else {
+        } else {
             this.brand = brand;
         }
         if (model == null) {
             this.model = "default";
-        }else {
+        } else {
             this.model = model;
         }
-        if (Double.compare(engineVolume,0) == 0) {
+        if (Double.compare(engineVolume, 0) == 0) {
             this.engineVolume = 1.5;
-        }else {
+        } else {
             this.engineVolume = engineVolume;
         }
         if (color == null) {
@@ -48,7 +73,7 @@ public class Car {
         this.productionYear = productionYear;
         if (productionCountry == null) {
             this.productionCountry = "default";
-        }else {
+        } else {
             this.productionCountry = productionCountry;
         }
         if (gears == null) {
@@ -65,6 +90,16 @@ public class Car {
             this.typeOfBody = "седан";
         } else {
             this.typeOfBody = typeOfBody;
+        }
+        if (key == null) {
+            this.key = new Key();
+        } else {
+            this.key = key;
+        }
+        if (insurance == null) {
+            this.insurance = new Insurance();
+        } else {
+            this.insurance = insurance;
         }
         this.seatsCount = seatsCount;
         this.summerTyres = summerTyres;
@@ -86,10 +121,20 @@ public class Car {
                 "седан",
                 "x000xx000",
                 5,
-                true);
+                true,
+                new Key(),
+                new Insurance());
     }
 
-   public void printOut() {
+    public Key getKey() {
+        return key;
+    }
+
+    public void setKey(Key key) {
+        this.key = key;
+    }
+
+    public void printOut() {
         System.out.println(brand + " " + model + ", " + productionYear + " год выпуска, " + "Sborka in " + productionCountry + ", Cvet: " + color + ", Ob'em dvigatelya " + engineVolume);
     }
 
@@ -120,7 +165,6 @@ public class Car {
             this.gears = gears;
         }
     }
-
     public String getRegNumber() {
         return regNumber;
     }
@@ -165,23 +209,71 @@ public class Car {
         return seatsCount;
     }
 
-    public void changeTyres(){
+    public void changeTyres() {
         summerTyres = !summerTyres;
     }
+
 
     public boolean isCorrectRegNumber() {
         //x000xx0000
         if (regNumber.length() != 9) {
             return false;
         }
-        char [] chars = regNumber.toCharArray();
+        char[] chars = regNumber.toCharArray();
         if (!Character.isAlphabetic(chars[0]) || !Character.isAlphabetic(chars[4]) || !Character.isAlphabetic(chars[5])) {
             return false;
         }
         if (!Character.isDigit(chars[1]) || !Character.isDigit(chars[2]) || !Character.isDigit(chars[3])
-        || !Character.isDigit(chars[6])  || !Character.isDigit(chars[7]) || !Character.isDigit(chars[8])) {
+                || !Character.isDigit(chars[6]) || !Character.isDigit(chars[7]) || !Character.isDigit(chars[8])) {
             return false;
         }
         return true;
     }
+
+    public static class Insurance {
+    private final LocalDate expireDate;
+    private final double cost;
+    private final String number;
+
+        public Insurance(LocalDate expireDate, double cost, String number) {
+            if (expireDate == null) {
+                this.expireDate = LocalDate.now().plusDays(365);
+            } else {
+                this.expireDate = expireDate;
+            }
+            this.cost = cost;
+            if (number == null) {
+                this.number = "123456789";
+            } else {
+                this.number = number;
+            }
+
+        }
+        public Insurance() {
+            this(null, 10000, null);
+        }
+        public LocalDate getExpireDate() {
+            return expireDate;
+        }
+
+        public double getCost() {
+            return cost;
+        }
+
+        public String getNumber() {
+            return number;
+        }
+        public void checkExpireDate() {
+            if (expireDate.isBefore(LocalDate.now()) || expireDate.isEqual(LocalDate.now())) {
+                System.out.println("Нужно срочно оформлять страховку");
+            }
+        }
+        public void checkNumber() {
+            if (number.length() != 9) {
+                System.out.println("Номер страховки неккоректен !");
+            }
+        }
+
+    }
+
 }
