bab5b69b4dbbf12173d48b5ffe910b2f3804766e
PDemenkov <guapnik@gmail.com>
2022-10-20 22:13:50 +0300

    V.2

---
+++ src/Car.java
@@ -0,0 +1,210 @@
+import java.time.LocalDate;
+
+public class Car {
+    private final String brand;
+    private final String model;
+    private float engineVolume;
+    private String color;
+    private final int productionYear;
+    private final String productionCounty;
+
+    private String gearBox;
+    private final String bodyType;
+    private String regNumber;
+
+    private final int placeCount;
+    private boolean winterTires;
+
+    private  Key key;
+    private  Insurance insurance;
+
+    public Car(String brand,
+               String model,
+               float engineVolume,
+               String color,
+               int productionYear,
+               String productionCounty,
+               String gearBox,
+               String bodyType,
+               String regNumber,
+               int placeCount,
+               boolean winterTires, Key key, Insurance insurance) {
+        this.brand = ValidationUtils.validOrDefault(brand, "default");
+        this.model = ValidationUtils.validOrDefault(model, "default");
+        this.productionYear = productionYear >= 0 ? productionYear : 2000;
+        this.productionCounty = ValidationUtils.validOrDefault(productionCounty, "default");
+        this.bodyType = ValidationUtils.validOrDefault(bodyType, "type");
+        this.placeCount = Math.max(placeCount, 1);
+        this.key = key;
+        this.insurance = insurance;
+        setEngineVolume(engineVolume);
+        setColor(color);
+        setGearBox(gearBox);
+        setRegNumber(regNumber);
+        setWinterTires(winterTires);
+    }
+
+    public void setSeasonTires() {
+        int currentMonth = LocalDate.now().getMonthValue();
+        this.winterTires = currentMonth <= 4 || currentMonth >= 11;
+    }
+
+    public boolean isRegNumberValid() {
+        if (this.regNumber.length() != 9) {
+            return false;
+        }
+        char[] regNumberChars = this.regNumber.toCharArray();
+        return isNumberLetter(regNumberChars[0])
+                && isNumber(regNumberChars[1])
+                && isNumber(regNumberChars[2])
+                && isNumber(regNumberChars[3])
+                && isNumberLetter(regNumberChars[4])
+                && isNumberLetter(regNumberChars[5])
+                && isNumber(regNumberChars[6])
+                && isNumber(regNumberChars[7])
+                && isNumber(regNumberChars[8]);
+
+    }
+
+    private boolean isNumber(char symbol) {
+        return Character.isDigit(symbol);
+    }
+
+    private boolean isNumberLetter(char symbol) {
+        String allowedSymbols = "ABEKHMHORCTYX";
+        return allowedSymbols.contains("" + symbol);
+    }
+
+    public void setWinterTires(boolean winterTires) {
+        this.winterTires = winterTires;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public float getEngineVolume() {
+        return engineVolume;
+    }
+
+    public void setEngineVolume(float engineVolume) {
+        this.engineVolume = engineVolume > 0.0f ? engineVolume : 1.5f;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = ValidationUtils.validOrDefault(color, "white");
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public String getProductionCounty() {
+        return productionCounty;
+    }
+
+    public String getGearBox() {
+        return gearBox;
+    }
+
+    public void setGearBox(String gearBox) {
+        this.gearBox = ValidationUtils.validOrDefault(gearBox, "default");
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    public String getRegNumber() {
+        return regNumber;
+    }
+
+    public void setRegNumber(String regNumber) {
+        this.regNumber = ValidationUtils.validOrDefault(regNumber, "default");
+    }
+
+    public int getPlaceCount() {
+        return placeCount;
+    }
+
+    public boolean isWinterTires() {
+        return winterTires;
+    }
+
+    public void winterTires(boolean winterTires) {
+        this.winterTires = winterTires;
+    }
+
+    public Key getKey() {
+        return key;
+    }
+
+    public Insurance getInsurance() {
+        return insurance;
+    }
+
+    public void setKey(Key key) {
+        this.key = key;
+    }
+
+    public void setInsurance(Insurance insurance) {
+        this.insurance = insurance;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", engineVolume=" + engineVolume +
+                ", color='" + color + '\'' +
+                ", productionYear=" + productionYear +
+                ", productionCounty='" + productionCounty + '\'' +
+                ", gearBox= " + gearBox +
+                ", bodyType= " + bodyType +
+                ", regNumber= " + regNumber +
+                " " +
+                ", placeCout= " + placeCount+
+                ", winterTires= " + winterTires +
+                ", key= " + key +
+                ", insurance= " + insurance +
+                '}';
+    }
+
+    public static class Key {
+private final boolean remoteEngineStart;
+private final boolean keylessAccess;
+
+        public Key(boolean remoteEngineStart, boolean keylessAccess) {
+            this.remoteEngineStart = remoteEngineStart;
+            this.keylessAccess = keylessAccess;
+        }
+    }
+
+    public static class Insurance {
+        private final LocalDate validUntil;
+        private final float cost;
+        private final String number;
+
+        public Insurance(LocalDate validUntil, float cost, String number) {
+            this.validUntil = validUntil !=null ? validUntil : LocalDate.now().plusDays(10);
+            this.cost = Math.max(cost,1f);
+            this.number = ValidationUtils.validOrDefault(number,"default");
+        }
+
+        public boolean ilNumberValid() {
+            return number.length() ==9;
+        }
+        public  boolean isInsuranceValid() {
+            return LocalDate.now().isBefore(this.validUntil);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import javax.security.auth.kerberos.KerberosTicket;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Car lada = new Car("Lada",
+                "Granta",
+                1.7f,
+                "yellow",
+                2015,
+                "Russia",
+                "Mex",
+                "sedan",
+                "s456vf344",
+                4,
+                true,
+                null, null);
+        Car.Key ladaKey = new Car.Key(true,true);
+        lada.setKey(ladaKey);
+//        Car.Insurance ladaINsurance = new Car.Insurance(2023.04.04, 11,10000f,4); Как ввести ?//        Car audi = new Car("Audi","A8 50 L TDI",3.0f,"black",2020,"Germany");
+//        Car bmw = new Car("Bmw","Z8",2.4f,"black",2021,"Germany");
+//        Car kia = new Car("Kia","Sportage",-1f,"",2018,"South Korea");
+//        Car hyundai  = new Car("Hyundai ","Avante",1.6f,"",2016,"South Korea");
+
+        System.out.println(lada);
     }
 }
